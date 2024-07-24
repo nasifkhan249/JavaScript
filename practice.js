f37300ddb3b5d8c8d351638d22f1cc5143168148
Nasif Mustufa Khan <nasifkhan994@gmail.com>
2024-07-24 13:41:35 +0600

    updated

--- practice.js
+++ practice.js
@@ -1,5 +1,5 @@
-let x=1;
-let y = 'ab10';
+// let x=1;
+// let y = 'ab10';
 
 // console.log(typeof (x+y));//110 by implicite and type is string;
 
@@ -16,6 +16,54 @@ let y = 'ab10';
 //console.log(x+y);//1ab10 by implicite and type is String;jodi akti value number and arekti value jodi string hoy and ai dui ti value ke ak shate + operator dara jug kora hoy tobe javaScript number a thaka value tike atomitically string a convert kore.jodi second value er samne jai tahok nh keno + operator thakle javaScript aitke string a convert korbe.ar duti value jodi string hoy tokhon JavaScript duiti value er modde concate kore thake.
 
 
-//console.log(typeof(x-y));//NAN by implicite and type hobe number.JavaScript implicite vabe second string value ke number a convert korte parche nh karon JavaScript protom value number payeche second value string payeche er por jokhon console korte geche tokhon dekeche je number thake string ke minus kora jai nh tokon JavaScript second value check kore je second value numeric number kinah check korbe jodi numeric number hoy tobe second value ke number a convert kore then output thik ashe jemon tah uporer second number example er khetre jemopn tah hoyeche.
+//console.log(typeof(x-y));//NAN by implicite and type hobe number.JavaScript implicite vabe second string value ke number a convert korte parche nh karon JavaScript protom value number payeche second value string payeche er por jokhon console korte geche tokhon dekeche je number thake string ke minus kora jai nh tokon JavaScript second value check kore je second value numeric number kinah check korbe jodi numeric number hoy tobe second value ke number a convert kore then output thik ashe jemon tah uporer second number example er khetre jemopn tah hoyeche.ar jodi numeric number nh hoy tahole output NaN ashbe.
 
+//console.log(x-parseInt(y));//NaN by exiplite .output a NaN ashar karon holo second value ti numeric number nh.karon second value te protome caracter tarpor number jar karone javaScript second value ke numeric number a convert korte nh pare output NaN aise.ar jodi caracter gulu number a sesh jodi thakto tokhon exiplite vabe second value ke number a convert kora jeto and output ashto numbe type.jemon tah nicher example a dewa holo.
+
+
+// let x = 10;
+// let y = '9ab';
+
+// let z = parseInt(y);//ai jaigah te javaScript parseInt er maddome string value ke number a convert koreche exiplite vabe.
+
+// y=z;//jokhon y er value ke number convert korar por aitike abar y variable re-assaign kora hoyeche
+
+// console.log(typeof(x-y));//1 by exiplite.type number
+
+// let x = !'hello' || !null;
+// console.log(x);
+
+// let x;
+// if(x='hello' || !null){
+//     console.log(x);
+// }
+
+
+
+// let x = 19.87654321;
+// console.log(x);
+// let z=Math.floor(x);
+// console.log(z);
+// x=z;
+// console.log(x);
+
+
+// let x=10.99;
+// console.log(Math.floor(x));//math.floor er kaj holo aiti doshomik er porer ongshotike bad diye dibe.
+
+// let x;
+
+// if(x='hello' || '10'){
+//     let y = 10.99;
+//    return console.log(typeof Math.floor(y));
+// }
+
+
+let x =10;
+
+if(Number){
+    
+
+    console.log(Math.pow(x,3));
+}
 
